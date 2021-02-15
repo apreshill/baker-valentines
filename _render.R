2dac91c19686a7bbdd8945d8f97a540ef971f914
Alison Hill <apreshill@gmail.com>
2021-02-15 14:40:25 -0500

    Initial commit

---
+++ _render.R
@@ -0,0 +1,15 @@
+function(input, ...) {
+  library(dplyr)
+  bnames <- readr::read_csv("data/bakers.csv") %>%
+   mutate(send_to = purrr::map2(.x = baker_first, .y = baker_last,
+                          ~list(first = .x,
+                                last = .y)))
+  purrr::walk(
+  .x = bnames$send_to,
+  ~ rmarkdown::render(
+    input = input,
+    output_file = glue::glue("valentine-for-{.x$first}.html"),
+    params = list(send_to = {.x})
+    )
+  )
+}
