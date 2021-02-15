23e3d9356cb20ebb0fc23b1bf4e049d143de566f
Alison Hill <apreshill@gmail.com>
2021-02-15 14:51:14 -0500

    override sent from

--- _render.R
+++ _render.R
@@ -9,7 +9,8 @@ function(input, ...) {
   ~ rmarkdown::render(
     input = input,
     output_file = glue::glue("valentine-for-{.x$first}.html"),
-    params = list(send_to = {.x})
+    params = list(send_to = {.x},
+                  sent_from = list(first = "Alison", last = "Hill"))
     )
   )
 }
